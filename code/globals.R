e6efbe9b8140c8a48320082d5abf04f46c374155
cnluzon <cnluzon@gmail.com>
2021-05-26 18:02:19 +0200

    wflow_publish("./analysis/master_gene_table.Rmd", verbose = T)

--- code/globals.R
+++ code/globals.R
@@ -2,12 +2,18 @@ global_palette <- list(teal = "#008080",
                        coral = "#F08080")
 
 gl_condition_colors <- c(
-  "Naive_EZH2i" = "#5F9EA0",
+  "Naive_EZH2i" = "#76c6c7", # "#5F9EA0",
   "Naive_Untreated" = "#278b8b",
-  "Primed_EZH2i" = "#f47770",
+  "Primed_EZH2i" = "#f48c8c", #  "#f47770",
   "Primed_Untreated" = "#f44b34"
 )
 
 theme_default <- function(base_size = 12) {
   theme_classic(base_size = base_size)
+}
+
+
+my_svg <- function(file, width, height) {
+  library(svglite)
+  svglite(file = file, width = width, height = height, bg = "white")
 }
--- code/ucsc_annotations.R
+++ code/ucsc_annotations.R
@@ -1,3 +1,7 @@
+library(org.Hs.eg.db)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+library(biomaRt)
+
 #' Get GRanges with gene symbols for UCSC hg38
 #'
 #' @return A GRanges where name field is Gene symbol
@@ -17,6 +21,53 @@ genes_hg38 <- function() {
   genes$name <- unname(gene_id_to_name[genes$gene_id])
 
   sort(genes[!is.na(genes$name),])
+}
+
+trx_ensembl_to_refseq <- function(gene_id) {
+  ensembl <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
+  result <- getBM(attributes=c("refseq_mrna", "ensembl_transcript_id", "hgnc_symbol"), filters = "ensembl_transcript_id", values = gene_id, mart= ensembl)
+  result
+}
+
+canonical_genes_hg38 <- function(file_ref_flat, file_canonical) {
+  # These files are in the default format I got
+  # refFlat.txt from UCSC annotation:
+  # knownCanonical.txt from Ensembl
+  ref_flat <- read.table(file_ref_flat, sep = "\t")
+  canonical <- read.table(file_canonical, sep = "\t")
+
+  # remove the version
+  canonical_ids <- gsub("[[:punct:]][0-9]+", "", canonical[, 5])
+
+  refseq_ids <- trx_ensembl_to_refseq(canonical_ids)
+
+  ref_flat <- ref_flat[, c(1:6)]
+  columns <- c("name", "id", "chr", "strand", "start", "end")
+  colnames(ref_flat) <- columns
+
+
+  canonical_flat <- ref_flat[ref_flat[, "id"] %in% refseq_ids$refseq_mrna, ]
+
+  # I still seem to need to select one each, because in ensembl_to_refseq
+  # there are sometimes 1:n correspondences.
+  canonical_flat$length <- canonical_flat$end - canonical_flat$start
+  canonical_flat <- canonical_flat %>% group_by(name) %>% slice_max(length, n = 1, with_ties = F)
+
+  canonical_genes <- unique(canonical_flat$name)
+
+  not_genes <- setdiff(unique(ref_flat[, "name"]), canonical_genes)
+
+  non_canonical_genes <- ref_flat %>% filter(!name %in% canonical_genes)
+  non_canonical_genes$length <- non_canonical_genes$end - non_canonical_genes$start
+  non_canonical_genes <- non_canonical_genes %>% group_by(name) %>% slice_max(length, n = 1, with_ties = F)
+
+  all <- rbind(canonical_flat[, columns], non_canonical_genes[, columns]) %>% arrange(name)
+  all$id <- NULL
+  makeGRangesFromDataFrame(all, keep.extra.columns = T)
+}
+
+genes_from_rnaseq <- function(refgene) {
+  table <- read.table(refgene, sep = "\t")
 }
 
 #' Get UCSC hg38 genes by gene symbol
