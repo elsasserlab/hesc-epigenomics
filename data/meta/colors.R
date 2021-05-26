9e8b1a9327a1b3b4c38677f4bb346f610b402aa3
cnluzon <cnluzon@gmail.com>
2021-05-26 18:31:19 +0200

    Metadata folder added

---
+++ data/meta/colors.R
@@ -0,0 +1,24 @@
+# Colors for the paper
+
+# Naïve: Teal - #008080 or Cadetblue #5F9EA0
+# Primed: Coral - #F08080 or f47770
+
+# H3K27me3 Naive: # 3e5aa8 
+# H3K27me3 Naive + EZH2i:  #092ba8
+
+# H3K27me3 Primed: 5d9ddd
+# H3K27me3 Primed + EZH2i:  #006bdd
+
+# H3K4me3 Naive: b64c28
+# H3K4me3 Naive + EZH2i: 6b2020
+
+# H3K4me3 Primed ffab45
+# H3K4me3 Primed + EZH2i e76e3b
+
+# H2Aub Naive: 8140c1
+# H2Aub Naive + EZH2i: 400c84
+
+# H2Aub Primed a07af0
+# H2Aub Primed + EZH2i a07af0
+
+# Highlight: #c31a1f
